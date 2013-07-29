78345e27aad4fa3c722129eeca49223e9c20d94d
crbonilha <cristhian.bonilha@hotmail.com>
2013-07-29 08:54:18 -0300

    Create gerador_revisao.cpp

---
+++ gerador_revisao.cpp
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// numero de casos de teste gerados
+const int CASOS = 30;
+
+int main() {
+  int d, size;
+	char n[110];
+	
+	FILE * out = fopen("revisao.in", "w");
+	
+	for(int k=0; k<CASOS; k++) {
+		d = (rand()%9)+1;
+		fprintf(out, "%d ", d);
+		
+		size = (rand()%90)+10;
+		for(int i=0; i<size; i++) {
+			n[i] = (rand()%9) + '0';
+		}
+		n[size] = '\0';
+		
+		fprintf(out, "%s\n", n);
+	}
+}
