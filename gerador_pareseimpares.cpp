8c1b153e8f238f65ac3685d04f79c8763028118d
crbonilha <cristhian.bonilha@hotmail.com>
2013-08-10 09:40:04 -0300

    Create gerador_pareseimpares.cpp

---
+++ gerador_pareseimpares.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int n;
+	
+	FILE * out = fopen("pareseimpares.in", "w");
+	
+	scanf("%d", &n);
+	fprintf(out, "%d\n", n);
+	
+	while(n--) {
+		fprintf(out, "%d\n", rand()%100000);
+	}
+}
