661b78f1e3be00a56c67db8d210807af441eb4be
crbonilha <cristhian.bonilha@hotmail.com>
2014-02-04 10:54:09 -0200

    Create imagem.cpp

---
+++ imagem.cpp
@@ -0,0 +1,20 @@
+    /*
+     * Este é um trecho do código utilizado no exercício Imagem - [link]
+     * Caso haja dúvidas, poste um comentário em [link]
+     */
+    
+    // processando
+		n = a/n;
+		m = b/m;
+		for(int i=0; i<a; i++) {
+			for(int j=0; j<b; j++) {
+				saida[i][j] = matriz[i/n][j/m];
+			}
+			saida[i][b] = 0;
+		}
+		
+		// imprimindo
+		for(int i=0; i<a; i++) {
+			printf("%s\n", saida[i]);
+		}
+		printf("\n");
